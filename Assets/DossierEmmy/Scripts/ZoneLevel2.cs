9c64372d0ff9adc7409d0a0996cdd3a45fe44c46
EmmyFortin <fortin.emmy@gmail.com>
2024-05-20 16:59:58 -0400

    sa fonctionne

--- Assets/DossierEmmy/Scripts/ZoneLevel2.cs
+++ Assets/DossierEmmy/Scripts/ZoneLevel2.cs
@@ -10,12 +10,15 @@ public class ZoneLevel2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         _levelManager = LevelManager.Instance;
     }
 
 
  private void OnTriggerEnter(Collider other)
- {
+
+ { 
+    Debug.Log("allo: ");
     _levelManager.LoadAsyncScene(LevelManager.Scene.Salle2);
  }
 }
