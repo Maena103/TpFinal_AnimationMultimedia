69d5a10e30c3848640a17c8001c77263a9d9fb51
EmmyFortin <fortin.emmy@gmail.com>
2024-05-22 18:16:40 -0400

    tant pis pour l'attaque

--- Assets/DossierEmmy/Scripts/Agent.cs
+++ Assets/DossierEmmy/Scripts/Agent.cs
@@ -7,50 +7,80 @@ public class Agent : MonoBehaviour
 {
     [SerializeField] private GameObject _joueur;
     [SerializeField] private NavMeshAgent _agent;
-  private LevelManager _levelManager;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AudioSource _attackAudioSource;
+    [SerializeField] private float _attackRange = 2.0f;
 
-    // Position du joueur que l'agent doit suivre
+    private LevelManager _levelManager;
     private Vector3 _positionJoueur;
-    
-    // Pour faire le changement de scene 
-    //[SerializeField] private GestionnaireScenes  _gestionScenes;
+    private bool _isAttacking = false;
 
-    // Pour les scriptable objects 
-    [SerializeField] private InfosJoueurs _infosJoueurs;
-    [SerializeField] private InfosNiveau _infosDuNiveau;
-
-      void Start()
+    void Start()
     {
         _levelManager = LevelManager.Instance;
     }
+
     void Update()
     {
         BougerAgent();
+        CheckAttack();
     }
 
-    void BougerAgent(){
-        // Dit à l'agent d'aller à la position du joueur
-        _positionJoueur = _joueur.transform.position;
-        _agent.SetDestination(_positionJoueur);
-    }
-
-     private void OnTriggerEnter(Collider other){
- 
-        // Debug pour savoir la cause de la mort 
-      
-
-        if(other.gameObject.tag == "Player"){
-             //Pour indiquer que le joueur est mort à cause de l'agent sois le bateau sur l'interface final de défaite
-           // _infosJoueurs._causeMort = ("Bateau");
-            _levelManager.LoadAsyncScene(LevelManager.Scene.SceneMort);
-             //Pour effectuer le changement de scene
-         //   _gestionScenes.SceneSuivante();
-             // Indique s'il y a une défaite ou une victoire dans se cas, c'est une défaite et vient changer la valeur de la booléen dans infosNiveau 
-             //ce qui permet de changer l'interface final dépendement si il y a une victoire ou une défaite
-          //  _infosDuNiveau._victoirePartie = false;
-            //_infosDuNiveau._defaitePartie = true;
-           
+    void BougerAgent()
+    {
+        if (!_isAttacking)
+        {
+            _positionJoueur = _joueur.transform.position;
+            _agent.SetDestination(_positionJoueur);
         }
-   
-  }
+    }
+
+    void CheckAttack()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, _joueur.transform.position);
+        if (distanceToPlayer <= _attackRange)
+        {
+            if (!_isAttacking)
+            {
+                StartCoroutine(AttackPlayer());
+            }
+        }
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        _isAttacking = true;
+        _agent.isStopped = true;
+
+        // Jouer l'animation d'attaque
+        _animator.SetTrigger("Attack");
+
+        // Jouer le son d'attaque
+        _attackAudioSource.Play();
+
+        // Attendre la fin de l'animation d'attaque
+        float attackAnimationLength = _animator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(attackAnimationLength);
+
+        // Attendre la fin du son d'attaque (si nécessaire)
+        float attackSoundLength = _attackAudioSource.clip.length;
+        yield return new WaitForSeconds(attackSoundLength);
+
+        // Attendre 2 secondes supplémentaires après l'animation et le son
+        yield return new WaitForSeconds(2.0f);
+
+        // Effectuer les actions après l'attaque
+        OnAttackCompleted();
+
+        _isAttacking = false;
+        _agent.isStopped = false;
+    }
+
+    private void OnAttackCompleted()
+    {
+        // Ici vous pouvez ajouter des actions après l'attaque, comme infliger des dégâts au joueur
+        _levelManager.LoadAsyncScene(LevelManager.Scene.SceneMort);
+    }
+
+
 }
